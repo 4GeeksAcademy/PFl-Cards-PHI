dbb16b2b38443b8192d668718254256b57d22f03
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-08-29 09:59:03 +0000

    [feat:]New folder utils in front, added apiFetch file

---
+++ src/front/utils/apiFetch.jsx
@@ -0,0 +1,24 @@
+import { toast } from "react-toastify";
+
+export const apiFetch = async (url, options = {}, navigate) => {
+  const token = localStorage.getItem("token");
+
+  const resp = await fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": "application/json",
+    },
+  });
+
+  // Si el token expiró o es inválido
+  if (resp.status === 401) {
+    toast.error("Tu sesión ha caducado. Vuelve a iniciar sesión.");
+    localStorage.removeItem("token");
+    navigate("/login");
+    return null; //  para que no intentes leer json
+  }
+
+  return resp;
+};
